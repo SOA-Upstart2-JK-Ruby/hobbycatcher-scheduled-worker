4509650ede2bbd03a9d11cead2798ff3562bc136
Karen Chiu <cola871225@gmail.com>
2021-11-14 20:37:07 +0800

    Value
    Still not finished...

---
+++ app/domain/suggestions/entities/hobby_suggestions.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module HobbyCatcher
+  module Entity
+    # Aggregate root for suggestions domain
+    class HobbySuggestions < SimpleDelegator
+
+      def hobby
+        
+      end
+    end
+  end
+end
+  
--- app/domain/suggestions/values/test_answer.rb
+++ app/domain/suggestions/values/test_answer.rb
@@ -24,28 +24,32 @@ module HobbyCatcher
         class Type
           include UserAnswer
           def type_ans
-            @type_ans
+            @type_ans = 'dynamic' if answer_t = 1
+            @type_ans = 'static' if answer_t = 2
           end
         end
   
         class Difficulty
           include UserAnswer
           def difficulty_ans
-            @difficulty_ans
+            @difficulty_ans = 'high' if answer_d = 1
+            @difficulty_ans = 'low' if answer_d = 2
           end
         end
 
         class FreeTime
           include UserAnswer
           def freetime_ans
-            @freetime_ans
+            @freetime_ans = 'long' if answer_f = 1
+            @freetime_ans = 'short' if answer_f = 2
           end
         end
 
         class Mood
           include UserAnswer
           def mood_ans
-            @mood_ans
+            @mood_ans = 'up' if answer_m = 1
+            @mood_ans = 'down' if answer_m = 2
           end
         end
       end
