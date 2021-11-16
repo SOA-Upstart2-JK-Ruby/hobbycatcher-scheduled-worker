18f8f92ff2e5c82af8625d1c1d94952b12656036
Karen Chiu <cola871225@gmail.com>
2021-11-16 20:02:23 +0800

    pull develop

--- app/domain/suggestions/entities/hobby_suggestions.rb
+++ app/domain/suggestions/entities/hobby_suggestions.rb
@@ -4,6 +4,14 @@ module HobbyCatcher
   module Entity
     # Aggregate root for suggestions domain
     class HobbySuggestions < SimpleDelegator
+      attr_reader :hobby, :answers, :create_time, :count
+
+      def initialize(hobby:, answers:, create_time:, count:)
+        @hobby = hobby
+        @answers = answers
+        @create_time = create_time
+        @count = count
+      end
     end
   end
 end
