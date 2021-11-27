5e6af11625af9ebe1e09f8915d761f7670a8b26c
ShihChu <juju0411tw@gmail.com>
2021-11-27 14:59:22 +0800

    Adjust history page

--- app/presentation/view_objects/category.rb
+++ app/presentation/view_objects/category.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'course'
 
 module Views
   # View for a single category entity
@@ -19,20 +18,6 @@ module Views
 
     def name
       @category.name
-    end
-
-    def courses
-      @category.owned_courses.map { |course| Course.new(course) }
-    end
-
-    def courses_each
-      courses.each do |course|
-        yield course
-      end
-    end
-
-    def courses_any?
-      courses.any?
     end
   end
 end
