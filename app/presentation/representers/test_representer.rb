45d4f2695c0768d2acf6d327ca616a7b9b36af3e
Karen Chiu <cola871225@gmail.com>
2021-12-03 15:48:54 +0800

    test: routing, service, representers, spec

---
+++ app/presentation/representers/test_representer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'roar/decorator'
+require 'roar/json'
+
+module HobbyCatcher
+  module Representer
+    # Represents test information about test page
+    class Test < Roar::Decorator
+      include Roar::JSON
+      property :description
+      property :answerA
+      property :answerB
+      property :button_name
+    end
+  end
+end
--- spec/tests/acceptance/api_spec.rb
+++ spec/tests/acceptance/api_spec.rb
@@ -23,6 +23,14 @@ describe 'Test API routes' do
     VcrHelper.eject_vcr
   end
   
+  describe 'Get test questions' do
+    it 'should successfully return test questions' do
+      HobbyCatcher::Service::ShowTest.new.call
+      get "/api/v1/test"
+      _(last_response.status).must_equal 200
+    end
+  end
+  
   describe 'Get suggestion information' do
     it 'should successfully return project lists' do
       HobbyCatcher::Service::ShowSuggestion.new.call(HOBBY_ID)
